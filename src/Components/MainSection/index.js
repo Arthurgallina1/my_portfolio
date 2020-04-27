5c706c2388db234f18f0a761dd38709294356f16
arthurgallina <arthur.gallina@yalo.com.br>
2020-04-27 15:27:56 -0300

    fix responsiveness

--- src/Components/MainSection/index.js
+++ src/Components/MainSection/index.js
@@ -3,29 +3,26 @@ import "./styles.scss";
 import Pic from "../../assets/cvemb.jpg";
 
 export default function MainSection() {
-    return (
-        <div className='container'>
-            <div className='main-box'>
-                <div className='main-box__left-side'>
-                    <h1>Hey, I'm Arthur.</h1>
-                    <h2>
-                        Software engineer from Floarinópolis, Brasil.
-                        <br /> I create custom solutions to{" "}
-                        <strong>help</strong>
-                        <br />
-                        businesses do better online.
-                    </h2>
-                    <button className='main-box__button large'>
-                        My Projects
-                    </button>
-                </div>
-                <div className='main-box__right-side'>
-                    <figure className='overlap-img'>
-                        {/* <source srcset={Pic} /> */}
-                        <img src={Pic} alt='' height={225} />
-                    </figure>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="main-box">
+        <div className="main-box__left-side">
+          <h1>Hey, I'm Arthur.</h1>
+          <h2>
+            Software engineer from Florianópolis, Brasil.
+            <br /> I create custom solutions to <strong>help</strong>
+            <br />
+            businesses do better online.
+          </h2>
+          <button className="main-box__button">My Projects</button>
         </div>
-    );
+        <div className="main-box__right-side">
+          <figure className="overlap-img">
+            {/* <source srcset={Pic} /> */}
+            <img src={Pic} alt="" height={225} />
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -2,45 +2,70 @@ import React from "react";
 import "./styles.scss";
 
 export default function Projects() {
-    return (
-        <div className='project-container'>
-            <h2>FEATURED PROJECTS</h2>
+  return (
+    <div className="project-container">
+      <h2>FEATURED PROJECTS</h2>
 
-            <div className='project-grid'>
-                <div className='project-card'>
-                    <div className='img-box'></div>
-                    <div className='description-box'>
-                        <h3>Project Name</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quos, distinctio?
-                        </p>
-                        <span>Stack: bla bla bla </span>
-                    </div>
-                </div>
-                <div className='project-card'>
-                    <div className='img-box'></div>
-                    <div className='description-box'>
-                        <h3>Project Name</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quos, distinctio?
-                        </p>
-                        <span>Stack: bla bla bla </span>
-                    </div>
-                </div>
-                <div className='project-card'>
-                    <div className='img-box'></div>
-                    <div className='description-box'>
-                        <h3>Project Name</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quos, distinctio?
-                        </p>
-                        <span>Stack: bla bla bla </span>
-                    </div>
-                </div>
-            </div>
+      <div className="project-grid">
+        <div className="project-card">
+          <div className="img-box">
+            <img
+              src=" https://api.adorable.io/avatars/250/abott@adorable.pngCopy to Clipboard
+"
+              alt=""
+            />
+          </div>
+          <div className="description-box">
+            <a href="/">Rate</a>
+            <p className="text-large">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              distinctio?
+            </p>
+            <br />
+            <span>
+              <strong>Stack:</strong> React, Redux, MongoDB{" "}
+            </span>
+          </div>
         </div>
-    );
+        <div className="project-card">
+          <div className="img-box">
+            <img
+              src=" https://api.adorable.io/avatars/250/abott@adorable.pngCopy to Clipboard
+"
+              alt=""
+            />
+          </div>
+          <div className="description-box">
+            <a href="/">Rate</a>
+            <p className="text-large">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              distinctio?
+            </p>
+            <br />
+            <span>
+              <strong>Stack:</strong> React, Redux, MongoDB{" "}
+            </span>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="img-box">
+            <img
+              src=" https://api.adorable.io/avatars/250/abott@adorable.pngCopy to Clipboard
+"
+              alt=""
+            />
+          </div>
+          <div className="description-box">
+            <a href="/">Rate</a>
+            <p className="text-large">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              distinctio?
+            </p>
+            <br />
+            <span>Stack: bla bla bla </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
