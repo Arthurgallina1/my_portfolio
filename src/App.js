ebd06be27053595edb1e9a13869b2188c4d42d85
arthurgallina <arthur.gallina@yalo.com.br>
2020-04-28 10:52:40 -0300

    add responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import Header from "./Components/Header";
 import MainSection from "./Components/MainSection";
 import Project from "./Components/Projects";
+import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Copyright from "./Components/Copyright";
 import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <MainSection />
-            <Project />
-            <About />
-            <Footer />
-            <Copyright />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <MainSection />
+      <Project />
+      <About />
+      <Footer />
+      <Copyright />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../logo.svg";
+import { FiMenu } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import "./index.scss";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <div className='nav__logo'>
-                <img src={logo} alt='' height={50} />
-            </div>
+  const [visible, setVisible] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-            <ul className='nav__list'>
-                <li className='nav__item'>About</li>
-                <li className='nav__item'>Project</li>
-                <li className='nav__item'>Contact</li>
-            </ul>
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+  const toggleNav = () => {
+    setVisible(!visible);
+  };
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+  return (
+    <header className="Header">
+      <img src={logo} alt="" height={50} />
+      {(!isSmallScreen || visible) && (
+        <nav className="Nav">
+          <a href="/">About</a>
+          <a href="/">Projects</a>
+          <a href="/">Contact</a>
         </nav>
-    );
+      )}
+      <button onClick={toggleNav}>
+        {visible ? (
+          <IoMdClose color={"#fff"} size={36} className="Burguer" />
+        ) : (
+          <FiMenu color={"#fff"} size={36} className="Burguer" />
+        )}
+      </button>
+    </header>
+  );
 }
