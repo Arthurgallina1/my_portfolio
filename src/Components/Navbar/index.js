12a35d164e793748f3eb744f93269fd6c0e13fd5
Arthur <arthur.gallina@gmail.com>
2020-04-28 19:13:36 -0300

    animate navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,48 +3,54 @@ import logo from "../../logo.svg";
 import { FiMenu } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
 import "./index.scss";
+import { CSSTransition } from "react-transition-group";
 
 export default function Navbar() {
-  const [visible, setVisible] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    mediaQuery.addListener(handleMediaQueryChange);
-    handleMediaQueryChange(mediaQuery);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
 
-    return () => {
-      mediaQuery.removeListener(handleMediaQueryChange);
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        };
+    }, []);
+    const toggleNav = () => {
+        setVisible(!visible);
     };
-  }, []);
-  const toggleNav = () => {
-    setVisible(!visible);
-  };
 
-  const handleMediaQueryChange = (mediaQuery) => {
-    if (mediaQuery.matches) {
-      setIsSmallScreen(true);
-    } else {
-      setIsSmallScreen(false);
-    }
-  };
-  return (
-    <header className="Header">
-      <img src={logo} alt="" height={50} />
-      {(!isSmallScreen || visible) && (
-        <ul className="Nav">
-          <li className="nav__item">About</li>
-          <li className="nav__item">Projects</li>
-          <li className="nav__item">Contact</li>
-        </ul>
-      )}
-      <button onClick={toggleNav}>
-        {visible ? (
-          <IoMdClose color={"#fff"} size={36} className="Burguer" />
-        ) : (
-          <FiMenu color={"#fff"} size={36} className="Burguer" />
-        )}
-      </button>
-    </header>
-  );
+    const handleMediaQueryChange = (mediaQuery) => {
+        if (mediaQuery.matches) {
+            setIsSmallScreen(true);
+        } else {
+            setIsSmallScreen(false);
+        }
+    };
+    return (
+        <header className='Header'>
+            <img src={logo} alt='' height={50} />
+            <CSSTransition
+                in={!isSmallScreen || visible} //(!isSmallScreen || visible)
+                timeout={450}
+                classNames='NavAnimation'
+                unmountOnExit
+            >
+                <ul className='Nav'>
+                    <li className='nav__item'>About</li>
+                    <li className='nav__item'>Projects</li>
+                    <li className='nav__item'>Contact</li>
+                </ul>
+            </CSSTransition>
+            <button onClick={toggleNav}>
+                {visible ? (
+                    <IoMdClose color={"#fff"} size={36} className='Burguer' />
+                ) : (
+                    <FiMenu color={"#fff"} size={36} className='Burguer' />
+                )}
+            </button>
+        </header>
+    );
 }
